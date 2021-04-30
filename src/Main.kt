4f3165a360f29e6a4d42d9173abd931b4cc9cd37
Andres Ortiz <andresortiz248@gmail.com>
2021-04-29 20:31:21 -0500

    Initial commit, function to show a menu

---
+++ src/Main.kt
@@ -0,0 +1,24 @@
+fun main() {
+    val menu = """
+        
+            :: Welcome to recipe maker ::
+             
+             Choose an option
+             1. Make a receipt 
+             2. Show my receipts
+             3. Exit   
+        """.trimIndent()
+
+
+    do{
+        println(menu)
+        val response:String? = readLine()
+
+        when(response){
+            "1" -> println("Option 1 chosen")
+            "2" -> println("Option 2 chosen")
+            "3" -> println("Exiting")
+            else -> println("Choose a valid option")
+        }
+    }while (!(response?.equals("3") ?: (response === null)))
+}
